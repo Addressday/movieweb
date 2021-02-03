133023521d889ca8ab854f3fdef76eceae7d3e22
root <root@localhost.localdomain>
2021-02-03 18:08:05 +0900

    3

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,7 +3,6 @@ import { Route, Switch } from "react-router-dom";
 import Auth from "../hoc/auth";
 // pages for this product
 import LandingPage from "./views/LandingPage/LandingPage.js";
-import GoogleButton from './views/GoogleLogin/GoogleButton'
 import LoginPage from "./views/LoginPage/LoginPage.js";
 import RegisterPage from "./views/RegisterPage/RegisterPage.js";
 import NavBar from "./views/NavBar/NavBar";
@@ -19,7 +18,7 @@ function App() {
       <div style={{ paddingTop: '69px', minHeight: 'calc(100vh - 80px)' }}>
         <Switch>
           <Route exact path="/" component={Auth(LandingPage, null)} />
-          <Route exact path="/login" component={Auth(GoogleButton, false)} />
+          <Route exact path="/login" component={Auth(LoginPage, false)} />
           <Route exact path="/register" component={Auth(RegisterPage, false)} />
           <Route exact path="/movie" component={Auth(PopularPage, null)} />
           <Route exact path="/movie/:movieId" component={Auth(MovieDetail, null)} />
